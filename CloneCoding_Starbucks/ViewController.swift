19840f1963e58b12f94d226fb75509a6c6bf2132
HANLeeeee <hanleeeee@naver.com>
2022-07-06 21:15:40 +0900

    StickyHeader 구현중

--- CloneCoding_Starbucks/ViewController.swift
+++ CloneCoding_Starbucks/ViewController.swift
@@ -7,14 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    //헤더뷰의 최대높이값과 최소높이값
+    let maxHeight: CGFloat = 400.0
+    let minHeight: CGFloat = 80.0
+
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var alphaHeaderView: UIView!
+    
+    @IBOutlet weak var mainScrollView: UIScrollView! {
+        didSet {
+            mainScrollView.contentInset = UIEdgeInsets(top: maxHeight, left: 0, bottom: 0, right: 0)
+             }
+        
+    }
+
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint! {
+        didSet {
+                heightConstraint.constant = maxHeight
+            }
+    }
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < 0 {
+            heightConstraint.constant = max(abs(scrollView.contentOffset.y), minHeight)
+        } else {
+            heightConstraint.constant = minHeight
+        }
+      }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainScrollView.delegate = self
         // Do any additional setup after loading the view.
     }
 
 
 }
-
 
