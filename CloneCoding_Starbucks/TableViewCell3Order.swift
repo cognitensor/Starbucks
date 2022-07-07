32eeea00fa502347d454c0b2c7e45d48e0f03dba
HANLeeeee <hanleeeee@naver.com>
2022-07-07 20:40:51 +0900

    Order화면 기본테이블뷰 생성

---
+++ CloneCoding_Starbucks/TableViewCell3Order.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewCell3Order.swift
+//  CloneCoding_Starbucks
+//
+//  Created by 하늘이 on 2022/07/07.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell3Order: UITableViewCell {
+    
+    @IBOutlet weak var orderContentView: UIView!
+    @IBOutlet weak var orderTitleImage: UIImageView!
+    @IBOutlet weak var orderTitleLabel: UILabel!
+    
+    //셀이 렌더링(그릴때) 될때
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        print("TableViewCell3Order -awakeFromNib")
+        
+        
+        orderTitleImage.layer.cornerRadius = orderTitleImage.frame.height/2
+        
+        
+    }
+    
+}
