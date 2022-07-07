3dcf0227030d9f0c479c59c9817abd566b3c62e1
HANLeeeee <hanleeeee@naver.com>
2022-07-07 17:22:54 +0900

    스티키헤더 성공

--- CloneCoding_Starbucks/ViewController.swift
+++ CloneCoding_Starbucks/ViewController.swift
@@ -14,29 +14,32 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var viewStartApp: UIView!
     
     //헤더뷰의 최대높이값과 최소높이값
-    let maxHeight: CGFloat = 400.0
-    let minHeight: CGFloat = 80.0
+
+    let maxHeight = (UIScreen.main.bounds.size.height)/2
+    let minHeight: CGFloat = 50.0
 
     @IBOutlet weak var mainScrollView: UIScrollView! {
         didSet {
             mainScrollView.contentInset = UIEdgeInsets(top: maxHeight, left: 0, bottom: 0, right: 0)
-             }
+        }
     }
-
-    @IBOutlet weak var heightConstraint: NSLayoutConstraint! {
+        
+    @IBOutlet weak var headerViewHeight: NSLayoutConstraint! {
         didSet {
-                heightConstraint.constant = maxHeight
-            }
+            headerViewHeight.constant = maxHeight
+        }
     }
+    
     
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.contentOffset.y < 0 {
-            //알파헤더뷰가 없어질떄까지 -400~0
-            heightConstraint.constant = max(abs(scrollView.contentOffset.y), minHeight)
-        } else {
-            //알파헤더뷰가 없어지고나서 0~
-            heightConstraint.constant = minHeight
+        print(scrollView.contentOffset.y)
+        let scrollOffset = scrollView.contentOffset.y
+        if scrollOffset > -maxHeight && scrollOffset < -48{
+            headerView.transform = CGAffineTransform(translationX: 0, y:abs(scrollOffset)-maxHeight)
+            
+            let persent = (-scrollOffset-100)/50
+            alphaHeaderView.alpha = persent
         }
       }
     
@@ -48,10 +51,15 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         viewStartApp.layer.shadowOffset = CGSize(width: 2, height: 3)   //그림자 크기
         viewStartApp.layer.shadowOpacity = 0.15                          //그림자 투명도 0이 투명 1이 불투명
-        viewStartApp.layer.shadowRadius = 3
+        viewStartApp.layer.shadowRadius = 5
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+ 
